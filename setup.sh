50554975abf8d382851a78912860acbfae54a16f
noboru-i <ishikura.noboru@gmail.com>
2022-11-16 23:52:26 +0900

    try push to doc branch

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 dart pub global activate -sgit https://github.com/rrousselGit/riverpod.git --git-path packages/riverpod_graph
-dart pub global run riverpod_graph:riverpod_graph > graph.md
+dart pub global run riverpod_graph:riverpod_graph > raw_output.md
+
+echo "\`\`\`mermaid" > graph.md
+sed -e '1,1d' raw_output.md >> graph.md
+echo "\`\`\`" >> graph.md
+
+rm raw_output.md
