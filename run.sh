e0fd1180f08c6f2c60930a358afabb4962d7c850
noboru-i <ishikura.noboru@gmail.com>
2022-12-10 16:09:10 +0900

    rename output file

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@ dart pub global activate -sgit https://github.com/rrousselGit/riverpod.git --git
 dart pub global run riverpod_graph:riverpod_graph $GITHUB_WORKSPACE > raw_output.md
 cat raw_output.md
 
-echo "\`\`\`mermaid" > graph.md
-sed -e '1,1d' raw_output.md >> graph.md
-echo "\`\`\`" >> graph.md
+echo "\`\`\`mermaid" > riverpod_graph.md
+sed -e '1,1d' raw_output.md >> riverpod_graph.md
+echo "\`\`\`" >> riverpod_graph.md
 
 rm raw_output.md
