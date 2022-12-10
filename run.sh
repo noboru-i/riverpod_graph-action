529325f1e0083e2785d69d973000181ac9391bd8
noboru-i <ishikura.noboru@gmail.com>
2022-12-10 14:12:18 +0900

    fix target path

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 dart pub global activate -sgit https://github.com/rrousselGit/riverpod.git --git-path packages/riverpod_graph
-dart pub global run riverpod_graph:riverpod_graph > raw_output.md
+dart pub global run riverpod_graph:riverpod_graph $GITHUB_WORKSPACE > raw_output.md
 
 echo "\`\`\`mermaid" > graph.md
 sed -e '1,1d' raw_output.md >> graph.md
