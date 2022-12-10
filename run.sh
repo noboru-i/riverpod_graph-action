0cb683644de660712b54787356ec963d8899d281
noboru-i <ishikura.noboru@gmail.com>
2022-12-10 15:07:41 +0900

    print result

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 dart pub global activate -sgit https://github.com/rrousselGit/riverpod.git --git-path packages/riverpod_graph
 echo $GITHUB_WORKSPACE
 dart pub global run riverpod_graph:riverpod_graph $GITHUB_WORKSPACE > raw_output.md
+cat raw_output.md
 
 echo "\`\`\`mermaid" > graph.md
 sed -e '1,1d' raw_output.md >> graph.md
