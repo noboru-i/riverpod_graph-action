da111ebdad5d8c1db40617dc7cdad82b5a3e1f9d
Noboru ISHIKURA <ishikura.noboru@gmail.com>
2022-12-10 16:19:20 +0900

    Refactor (#1)
    * test
    
    * print result
    
    * test directory
    
    * update README
    
    * remove unused prints
    
    * move flutter-action
    
    * rename output file
    
    * rename directories
    
    * update README

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 dart pub global activate -sgit https://github.com/rrousselGit/riverpod.git --git-path packages/riverpod_graph
 dart pub global run riverpod_graph:riverpod_graph $GITHUB_WORKSPACE > raw_output.md
+cat raw_output.md
 
-echo "\`\`\`mermaid" > graph.md
-sed -e '1,1d' raw_output.md >> graph.md
-echo "\`\`\`" >> graph.md
+echo "\`\`\`mermaid" > riverpod_graph.md
+sed -e '1,1d' raw_output.md >> riverpod_graph.md
+echo "\`\`\`" >> riverpod_graph.md
 
 rm raw_output.md
